94326c1e50b64d1fe3bd49103a0c8094f332b8c9
DevloperSujalPadoliya <padoliyasujal6@gmail.com>
2026-02-23 20:17:09 +0530

    Add files via upload

--- java/classOne.java
+++ java/classOne.java
@@ -1,6 +1,25 @@
 public class classOne {
 
-    public static void main(String[] args){
-     System.out.println("hello padmini");
+    public void main(String[]arg){
+        System.out.println("Hello");
+
+        // only send 10 digit int capacity is 10 digit 
+        int num = 1234567890; 
+        System.out.println(num);
+
+        char num1 = 'S';
+        System.out.println(num1);
+
+        // only send 5 digit capacity of 5 digit store 
+        short num2 = 11111;
+        System.out.println(num2);
+
+         // only send 3 digit capacity of 3 digit store 
+        byte num3 = 123;
+        System.out.println(num3);
+
+        // this is use to long number and the last added "L"
+        long num4 = 1234567891012l;
+        System.out.println(num4);
     }
-} 
+}
